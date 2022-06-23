56137f75b490430af53918c13ad4f5d0db788804
kobihall <kobihallacc@gmail.com>
2022-06-23 08:21:28 -0500

    Random utility functions

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -12,4 +13,14 @@ const double PI = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * PI / 180.0;
+}
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
